9f27da5c6b4df1ada30a2422dadf86d406498bd5
OmarKarkoura <80950624+OmarKarkoura@users.noreply.github.com>
2021-03-30 14:40:07 +0200

    I have spotted some errors which are now fixed, also some improvements for better readability

--- Data extraction-visualisation/FinalDataScript.py
+++ Data extraction-visualisation/FinalDataScript.py
@@ -26,17 +26,17 @@ def open_file(dataFrame):
 
 
 
-url_30K = ""
-url_300K = ""
-url_tempDepend = ""
-pd_30K_url = ""    # I use the data in this file for performing corrections
+url_30K = "/Users/omarkarkoura/Library/Mobile Documents/com~apple~CloudDocs/Project:Thesis/Project/SiC Experimental Data:Results/SiC3/Hyst at 30K/SiC3_hyst_30K_FinalData.csv"
+url_300K = "/Users/omarkarkoura/Library/Mobile Documents/com~apple~CloudDocs/Project:Thesis/Project/SiC Experimental Data:Results/SiC3/Hyst at 300K/SiC3_hyst_300K_FinalData.csv"
+url_tempDepend = "/Users/omarkarkoura/Library/Mobile Documents/com~apple~CloudDocs/Project:Thesis/Project/SiC Experimental Data:Results/SiC3/Temperature Dependance/SiC3_temp_dep_FinalData.csv"
+pd_30K_url = "/Users/omarkarkoura/Library/Mobile Documents/com~apple~CloudDocs/Project:Thesis/Project/SiC Experimental Data:Results/Pd_callibration/Hyst at 30K/callibration_data_30K_hyst_finalData.csv"    # I use the data in this file for performing corrections
 
 
-pd_30K = pd.read_csv(pd_300K_url)
-pd_30K = pd_300K[["Time", "Field (Oe)", "Temperature (K)", "Long Moment (emu)", "Long Scan Std Dev"]]  # Filter out only useful columns
+pd_30K = pd.read_csv(pd_30K_url)
+pd_30K = pd_30K[["Time", "Field (Oe)", "Temperature (K)", "Long Moment (emu)", "Long Scan Std Dev"]]  # Filter out only useful columns
 
 
-pd_30K['Field (Oe)'] = pd_300K['Field (Oe)']/10000
+pd_30K['Field (Oe)'] = pd_30K['Field (Oe)']/10000
 pd_30K.rename(columns={"Field (Oe)":"Field(T)"}, inplace=True)
 
 
@@ -56,8 +56,10 @@ pd_correction = pd_30K['Long Moment (emu)']/params[0]  # Values of actual field
 # Plotting the data with the fitted line
 plt.figure('Callibration')
 plt.title('Fitting function for the data points of Pd at 30K')
-plt.plot(pd_300K['Field(T)'], linear_func(pd_300K['Field(T)'], params[0]), label='Fitted function')
-plt.plot(pd_300K['Field(T)'], pd_300K['Long Moment (emu)'], label='Data points')
+plt.xlabel('Field (T)')
+plt.ylabel('Long Moment (emu)')
+plt.plot(pd_30K['Field(T)'], linear_func(pd_30K['Field(T)'], params[0]), label='Fitted function')
+plt.scatter(pd_30K['Field(T)'], pd_30K['Long Moment (emu)'], label='Data points')
 plt.show()
 
 
@@ -85,22 +87,54 @@ data_300K.rename(columns={"Field (Oe)":"Field(T)"}, inplace=True)
 
 # Plotting the temperature dependence curve
 plt.figure('Temperature Dependence')
-plt.scatter(data_tempDepend['Temperature (K)'], data_tempDepend['Long Moment (emu)'])
-plt.show()
-
-
-# Plotting data at 30K
-plt.figure('30K')
-plt.scatter(data_30K['Field(T)'], data_30K['Long Moment (emu)'], label='Data at 30K')
-plt.title('Magnetic moment dependence on field at 30K')
+plt.title('Temperature dependence at 5T')
+plt.plot(data_tempDepend['Temperature (K)'], data_tempDepend['Long Moment (emu)'],'-o')
 plt.xlabel('Field (T)')
-plt.ylabel('Long Magnetic Moment (emu)')
-plt.ylim(-0.00029,0.00029)
-plt.legend(loc='best')
+plt.ylabel('Long Moment (emu)')
 plt.show()
 
 
-#Comparing data at 30K & 300K. Moreover I have included in the same figure the observed ferromagnetic loop at 300K (should be zoomed by hand)
+# Plotting data at 30K with opening shown in separate plot
+fig_30K, ax_30K = plt.subplots(2,1)
+fig_30K.tight_layout()
+ax_30K[0].scatter(data_30K['Field(T)'],data_30K['Long Moment (emu)'],label='30K')
+ax_30K[0].set_ylim(-0.00029,0.00029)
+ax_30K[0].set_title('Magnetic moment dependence on field at 30K')
+ax_30K[0].set_xlabel('Field (T)')
+ax_30K[0].set_ylabel('Long Magnetic Moment (emu)')
+ax_30K[0].legend(loc='best')
+
+ax_30K[1].scatter(data_30K['Field(T)'],data_30K['Long Moment (emu)'],label='30K')
+ax_30K[1].set_xlabel('Field (T)')
+ax_30K[1].set_ylabel('Long Magnetic Moment (emu)')
+ax_30K[1].set_title('Observed Ferromagnetic behavior (opening) at 30K')
+ax_30K[1].set_ylim(-0.000038,0.000038)
+ax_30K[1].set_xlim(-0.15,0.15)
+ax_30K[1].legend(loc='best')
+plt.show()
+
+
+# Plotting data at 30K with opening shown in separate plot
+fig_300K, ax_300K = plt.subplots(2,1)
+fig_300K.tight_layout()
+ax_300K[0].scatter(data_30K['Field(T)'],data_30K['Long Moment (emu)'],label='300K')
+ax_300K[0].set_ylim(-0.00029,0.00029)
+ax_300K[0].set_title('Magnetic moment dependence on field at 300K')
+ax_300K[0].set_xlabel('Field (T)')
+ax_300K[0].set_ylabel('Long Magnetic Moment (emu)')
+ax_300K[0].legend(loc='best')
+
+ax_300K[1].scatter(data_30K['Field(T)'],data_30K['Long Moment (emu)'],label='300K')
+ax_300K[1].set_xlabel('Field (T)')
+ax_300K[1].set_ylabel('Long Magnetic Moment (emu)')
+ax_300K[1].set_title('Observed Ferromagnetic behavior (opening) at 300K')
+ax_300K[1].set_ylim(-0.000038,0.000038)
+ax_300K[1].set_xlim(-0.15,0.15)
+ax_300K[1].legend(loc='best')
+plt.show()
+
+
+#Comparing data at 30K & 300K including the ferromagnetic opening
 fig, ax = plt.subplots(2,1)
 fig.tight_layout()
 ax[0].scatter(data_300K['Field(T)'],data_300K['Long Moment (emu)'],label='300K')
@@ -112,10 +146,12 @@ ax[0].set_ylabel('Long Magnetic Moment (emu)')
 ax[0].legend(loc='best')
 
 ax[1].scatter(data_300K['Field(T)'],data_300K['Long Moment (emu)'],label='300K')
+ax[1].scatter(data_30K['Field(T)'],data_30K['Long Moment (emu)'],label='300K')
 ax[1].set_xlabel('Field (T)')
 ax[1].set_ylabel('Long Magnetic Moment (emu)')
-ax[1].set_title('Observed Ferromagnetic behavior at 300K')
-ax[1].set_ylim(-0.00029,0.00029)
+ax[1].set_title('Observed Ferromagnetic behavior (opening) at two different temperatures')
+ax[1].set_ylim(-0.000038,0.000038)
+ax[1].set_xlim(-0.15,0.15)
 ax[1].legend(loc='best')
 plt.show()
 
@@ -133,12 +169,12 @@ actualB_lineSubtracted = data_30K['Long Moment (emu)'] - params_actualB[0]*pd_co
 #Plotting the data at 30K for the set and actual fields for comparison
 fig, ax = plt.subplots(2,1)
 fig.tight_layout()
-ax[0].scatter(pd_correction, final_data_30K['Long Moment (emu)'],label='Actual Field')
+ax[0].scatter(pd_correction, data_30K['Long Moment (emu)'],label='Actual Field')
 ax[0].set_xlabel('Field (T)')
 ax[0].set_ylabel('Long Magnetic Moment (emu)')
 ax[0].legend(loc='best')
 
-ax[1].scatter(final_data_30K['Field(T)'], final_data_30K['Long Moment (emu)'], color='black', label='Set Field')
+ax[1].scatter(data_30K['Field(T)'], data_30K['Long Moment (emu)'], color='black', label='Set Field')
 ax[1].set_xlabel('Field (T)')
 ax[1].set_ylabel('Long Magnetic Moment (emu)')
 ax[1].legend(loc='best')
